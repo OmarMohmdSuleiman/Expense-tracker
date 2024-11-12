804b04c01c2fc38314316b5f157588a1cb0cc554
Omar <omarsuleiman739@gmail.com>
2024-11-12 18:14:55 +0200

    Move index.html to Expense-tracker folder

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+
+function App(){
+    
+    return (<div>
+    <Header />
+    <h1>Hello There !!!</h1>
+    
+    </div>);
+}
+
+export default App;

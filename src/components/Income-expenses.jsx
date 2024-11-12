4a3edad83d8a0f61e8c68de28710643c38a99734
Omar <omarsuleiman739@gmail.com>
2024-11-12 23:24:56 +0200

    Add income and expense props to display in Income-expenses component

--- src/components/Income-expenses.jsx
+++ src/components/Income-expenses.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function IncomeExpenses(){
+function IncomeExpenses(props){
     return (<div className="inc-exp-container">
         <div>
-          <h4>Income</h4>
+          <h4>{props.Income}</h4>
   <p className="money plus">0 $</p>
         </div>
         <div>
-          <h4>Expense</h4>
+          <h4>{props.Expense}</h4>
   <p className="money minus">0$</p>
         </div>
       </div>);

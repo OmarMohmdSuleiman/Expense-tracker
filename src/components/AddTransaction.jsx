b4fcb51aa3aafa4a963d02cf9a1ac455e872f6fe
Omar <omarsuleiman739@gmail.com>
2024-11-12 21:58:31 +0200

    Adjust all classes in the components into className

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -5,11 +5,11 @@ function AddTransaction(){
     <div><h3>Add new transaction</h3>
     <form >
       <div className="form-control">
-        <label for="text">Text</label>
+        <label htmlFor="text">Text</label>
         <input type="text"  placeholder="Enter text..." />
       </div>
       <div className="form-control">
-        <label for="amount">Amount <br />
+        <label htmlFor="amount">Amount <br />
           (negative - expense, positive - income)</label>
         <input type="number"  placeholder="Enter amount..." />
       </div>

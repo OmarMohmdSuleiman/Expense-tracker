d0590f25d0274f205711bbd396287d5b4f59262d
Omar <omarsuleiman739@gmail.com>
2024-11-12 22:07:47 +0200

    Add useState and onChange handler for input values for the transaction and it's amount

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 
 function AddTransaction(){
+
+  const [text,setText]=useState('');// set hook for the transaction
+  const [amount,setAmount]=useState(0);// set hook to track the amount
+  
+  //make function to handle current typed input (value)
+  function handleTransaction(e){
+    setText(e.target.value);
+  }
+  //make function to handle current typed amount (value)
+  function handleAmount(e){
+    setAmount(e.target.value);
+  }
     return ( 
     <div><h3>Add new transaction</h3>
     <form >
       <div className="form-control">
         <label htmlFor="text">Text</label>
-        <input type="text"  placeholder="Enter text..." />
+        <input type="text" value={text} onChange={handleTransaction} placeholder="Enter text..." />
       </div>
       <div className="form-control">
         <label htmlFor="amount">Amount <br />
           (negative - expense, positive - income)</label>
-        <input type="number"  placeholder="Enter amount..." />
+        <input type="number" value={amount} onChange={handleAmount} placeholder="Enter amount..." />
       </div>
       <button className="btn">Add transaction</button>
     </form></div>

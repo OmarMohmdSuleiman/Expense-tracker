79dbc8cbd25d30f2052d8ac753827ece5765f043
Omar <omarsuleiman739@gmail.com>
2024-11-13 17:40:28 +0200

    Set up PostgreSQL connection with environment variables using Pool

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public")); // Look inside the public folder
 
+const pool = new Pool({
+    user: process.env.PG_USER,        
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+  });
+
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
   })

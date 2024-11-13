b11397087cbeca43088a0c7b2353ac5fbbec3c0b
Omar <omarsuleiman739@gmail.com>
2024-11-13 17:24:06 +0200

    Run the express server on port 4000

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
+
+const app = express(); //Initialize the express app
+const port = 4000; // Port of the server
+
+env.config(); // Load the variables from .env
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public")); // Look inside the public folder
+
+// Start the Express server on port 4000
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
